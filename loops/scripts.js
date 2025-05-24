e951dc5a843a62b1b8b86f2116f28eaa3597b91b
DagimDev <simedagim121@gmail.com>
2025-05-24 11:28:07 +0300

    Loops in javascript

---
+++ loops/scripts.js
@@ -0,0 +1,10 @@
+console.log("JavaScript Loops")
+
+// JavaScript For Loop
+// ▪ For loops are commonly used to count a certain number of iterations to repeat a
+// block of code. Meaning, we use for loop when we want to execute the code for a
+// SPECIFIED amount of time
+// ▪ For loop Syntax:
+ for (initialization; condition; post/finalexpression) {
+// // statements (code block to be executed)
+ }
