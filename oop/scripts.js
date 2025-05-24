3b04759525a698f6003bcae5f50bf2febde44626
DagimDev <simedagim121@gmail.com>
2025-05-24 12:30:38 +0300

    Object created with litreral syntax

---
+++ oop/scripts.js
@@ -0,0 +1,27 @@
+console.log("Object oriented programming in JavaScript");
+
+// Object oriented programming in JavaScript
+
+// Example to describe a Car using the object-oriented data model
+    // ▪ Object: Let’sa assume we have a “car” object
+    // ▪ Properties of the object: model, make, color, doors
+    // ▪ Methods/Actions: Car Starts (startCar), Car Accelerates (accelerate), Car
+// Decelerates (decelerate)
+    // ▪ Events: enter a> startCar, up arrow > accelerate, down arrow > decelerate
+    // • Note: At this point, the computer would have some understanding of what
+// a car is. It still doesn't know everything about a car. For example, it doesn't
+// know it has windows or tire 
+
+// Object literal syntax: var object-name = {key1: value1, key2: value2};
+// o Example:
+let person = {
+firstName: 'Abebe',
+lastName: 'Kebede',
+someFunction: function () {
+return this.firstName;
+}
+};
+// o FirstName, lastName and someFunction are the properties of
+// person object
+// o Abebe, Kebede and function () {return this.firstName;} are
+// property values
