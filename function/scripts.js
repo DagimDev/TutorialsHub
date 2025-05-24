2d6b066ed51c1d2d73cef83954fce3fc4111729a
DagimDev <simedagim121@gmail.com>
2025-05-24 09:26:21 +0300

    Function declaration

---
+++ function/scripts.js
@@ -0,0 +1,7 @@
+console.log("Function")
+// Function declaration: Syntax for defining function with declaration
+function myFirstFunction (number1, number2) {
+return number1 + number2;
+}
+
+console.log(myFirstFunction(22, 3)) 
