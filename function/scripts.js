5961d5b5ef1952971e56e094b5b3b517907dd6da
DagimDev <simedagim121@gmail.com>
2025-05-24 09:34:33 +0300

    Function expression

--- function/scripts.js
+++ function/scripts.js
@@ -5,3 +5,15 @@ return number1 + number2;
 }
 
 console.log(myFirstFunction(22, 3)) 
+
+// Function expressions: Syntax for defining function with declaration
+const x = function (number3, number4){
+return number3 + number4
+};
+
+// After a function expression has been stored in a variable, the variable can
+// be used as a function. Functions stored in variables are anonymous or do
+// not need function names. They are always invoked (called) using the
+// variable name
+// • You can call/invoke function x above like this:
+// ▪ x (9, 11) // this will return 20, because of 9 + 11
