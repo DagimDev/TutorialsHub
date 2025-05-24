b807197dd8d86187871e15f094907821512800a6
DagimDev <simedagim121@gmail.com>
2025-05-24 10:36:32 +0300

    If statments

---
+++ ifStatments/scripts.js
@@ -0,0 +1,21 @@
+console.log("If Statments")
+
+// if statements
+// • Definition: An “if” statement is a programming conditional statement that we use to
+// specify a block
+// • Syntax: We start by using the key word "if", put the conditions within a bracket. Note that
+// if is in lowercase letters. Uppercase letters (If or IF) will generate a JavaScript error.
+// Syntax 1:
+if (condition) {
+// write the code you want to be executed if
+// the condition is true
+}
+Example:
+var pass = 50;
+var score = 17;
+if (score >= pass) {
+console.log("Hey you passed");
+}
+if (score <= pass) {
+console.log("You failed");
+}
