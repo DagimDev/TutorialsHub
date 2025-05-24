5bc42ea2076776f0c8b5011d92cbf2219ac73de1
DagimDev <simedagim121@gmail.com>
2025-05-24 11:28:29 +0300

    Loop scope

--- loops/scripts.js
+++ loops/scripts.js
@@ -8,3 +8,17 @@ console.log("JavaScript Loops")
  for (initialization; condition; post/finalexpression) {
 // // statements (code block to be executed)
  }
+
+ // 2 (Loop scope): If you use the var keyword to declare the
+// counter variable, the variable will have either function or global
+// scope. In other words, you can reference the counter variable after
+// the loop ends. However, if you use the let keyword to declare the
+// counter variable, the variable will have a blocked scope, which is
+// only accessible inside the loop. See the example below:
+for (let counter = 1; counter < 5; counter++)
+{
+console.log(counter); // prints numbers 1 to 4
+}
+ console.log(counter); //prints “ReferenceError:
+// counter is not defined” because we used let
+// to define counter
