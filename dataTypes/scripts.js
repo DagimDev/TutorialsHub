917d989b3d0f1cce01466dfb3dbbadc831c56299
DagimDev <simedagim121@gmail.com>
2025-05-26 22:53:27 +0300

    Primitive data stracture

--- dataTypes/scripts.js
+++ dataTypes/scripts.js
@@ -13,3 +13,32 @@ console.log("Data Type");
 
 // Data structure types: There are two types of data structure available for the programming
 // purpose, primitive data structure and non-primitive data structure. 
+
+
+
+// Primitive data structure (data types) in JavaScript: These data structures are the ones
+// that are predefined by almost all programing languages. Whether it is JavaScript, Python
+// or any other programing language, they know what these data types are. They know what
+// to do with them. Ex: If you tell a programing language to add two integer values, it knows
+// what to do with the integers. Primitive data types include:
+// ▪ Number/integer: which stores a range on mathematical integers
+// ▪ String: Strings are useful for holding data that can be represented in text form.
+// Strings are written with quotes. You can use single or double quotes.
+// ▪ Boolean: stores logical values that are either true or false.
+// ▪ Undefined: The undefined datatype, whose value is undefined is used to denote
+// an absence of a meaningful value. Do you remember when we said the datatype of
+// a variable that has no value assigned is 'undefined', that is what we mean by
+// undefined data type.
+// ▪ Symbol: was introduced in ECMAScript 2015, so just a few years ago. It's a very
+// peculiar data type. Once you create a symbol, its value is kept private and for
+// internal use. Symbol can be created using the factory function Symbol () which 
+// returns a Symbol. Every time you call the factory function, a new and unique
+// symbol is created.
+// ▪ Null: JavaScript defines that null is an empty object value. Null represents the
+// intentional absence of any object value. It is one of JavaScript's primitive values
+// and is treated as falsy for Boolean operations.
+// ▪ BigInt: Bigint value represent integers with arbitrary precision. provides a way to
+// represent whole numbers larger than 253
+// -1. So, it means, with BigInts, you can
+// safely store and operate on large integers even beyond the safe integer limit for
+// Numbers.
