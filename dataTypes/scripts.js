1c3d17492f5d269db96a01e112a906255816a33b
DagimDev <simedagim121@gmail.com>
2025-05-26 22:51:19 +0300

    Data Stracture

--- dataTypes/scripts.js
+++ dataTypes/scripts.js
@@ -1,1 +1,12 @@
-console.log("Data Type")
+console.log("Data Type");
+
+// What is data structure and why do we need to structure data?
+//  • Data structure: It is a particular way of storing, organizing and processing
+// data/information in a computer so that it can be retrieved and used most productively.
+//  ▪ The concept of data types is important in programming. That is why learning
+// programing language starts by understanding what data is and how it is structured
+// for effective use. To be able to operate on variables, it is important to know
+// something about the data type.
+//  ▪ Data structures are the building blocks of algorithms and computer programs. It is
+// a key topic when it comes to Software Engineering interview questions. Hence as
+// developers, we must have good knowledge about data structures
