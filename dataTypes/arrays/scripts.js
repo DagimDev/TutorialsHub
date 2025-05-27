2fecf59f6570bcfbd87929156b122efc18969bbb
DagimDev <simedagim121@gmail.com>
2025-05-27 23:10:10 +0300

    Declaring/creating an array

--- dataTypes/arrays/scripts.js
+++ dataTypes/arrays/scripts.js
@@ -4,3 +4,9 @@ console.log("Arrays")
 // array is a special variable, which can hold more than one value. The values in an array are
 // referred with a name and an index. Please note that an array can hold values that are of
 // different data type.
+
+// Declaring/creating an array: An array can hold many values under a single name and the
+// below examples show how one can create an array and add values into the array.
+// Sytax to declare an array: var arrayName = [ ];
+// Example: 
+var gradeResults = [ ];
