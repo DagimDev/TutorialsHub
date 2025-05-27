e910d2f36829bd7232c98e8a37b1f0a45b66af3e
DagimDev <simedagim121@gmail.com>
2025-05-27 23:09:30 +0300

    Array (definition)

---
+++ dataTypes/arrays/scripts.js
@@ -0,0 +1,6 @@
+console.log("Arrays")
+
+// Array (definition): It is the most used data type from the nonprimitive data structures. An
+// array is a special variable, which can hold more than one value. The values in an array are
+// referred with a name and an index. Please note that an array can hold values that are of
+// different data type.
