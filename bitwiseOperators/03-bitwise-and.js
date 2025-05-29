53ae95850f1ab13204fcd8e374773f4bbcea58df
DagimDev <simedagim121@gmail.com>
2025-05-29 22:43:25 +0300

    feat: Bitwise OR

--- bitwiseOperators/03-bitwise-and.js
+++ bitwiseOperators/03-bitwise-and.js
@@ -3,3 +3,9 @@
 
 // 5 & 3 → 0101 & 0011 = 0001 (1)
 // Use Case: Checking if a number is even/odd (n & 1).
+
+
+// 2. Bitwise OR (|)
+// Rule: 0 | 0 = 0, otherwise 1.
+// 5 | 3 → 0101 | 0011 = 0111 (7)
+// Use Case: Combining flags/permissions.
