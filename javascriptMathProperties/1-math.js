7e78c2d9e9f5b9105d63290e7382448e52de725f
DagimDev <simedagim121@gmail.com>
2025-06-01 18:28:28 +0300

    Math Properties (Constants)

---
+++ javascriptMathProperties/1-math.js
@@ -0,0 +1,11 @@
+// JavaScript Math Properties and Methods 
+// Math Properties (Constants)
+javascript
+Math.PI         // 3.14159... - Circle calculations, animations
+Math.E          // 2.718... - Exponential growth, natural logarithms
+Math.LN2        // 0.693... - Logarithmic calculations
+Math.LN10       // 2.302... - Logarithmic calculations
+Math.LOG2E      // 1.442... - Logarithm base conversions
+Math.LOG10E     // 0.434... - Logarithm base conversions
+Math.SQRT2      // 1.414... - Diagonal calculations
+Math.SQRT1_2    // 0.707... - Normalization factors
