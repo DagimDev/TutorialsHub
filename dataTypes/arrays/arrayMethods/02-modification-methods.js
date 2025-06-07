9533a45f13ca8780dfe6875a51023db7fbc11dab
DagimDev <simedagim121@gmail.com>
2025-06-07 21:50:27 +0300

    Modification method Push()

---
+++ dataTypes/arrays/arrayMethods/02-modification-methods.js
@@ -0,0 +1,17 @@
+push()
+// ➤ What it does:
+// Adds one or more elements to the end of an array.
+
+// ➤ Syntax:
+
+// array.push(element1, element2, ...);
+// ➤ Returns:
+// The new length of the array.
+
+// ➤ Mutates original array? ✅ Yes
+// ➤ Example:
+
+let colors = ['red', 'green'];
+colors.push('blue');
+
+console.log(colors); // ['red', 'green', 'blue']
