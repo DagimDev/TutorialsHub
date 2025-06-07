03a27de506838ef2a02c79b92e5ee0dad0a38cb9
DagimDev <simedagim121@gmail.com>
2025-06-07 22:58:23 +0300

    UM slice() : Returns a shallow copy of a portion of the array

--- dataTypes/arrays/arrayMethods/03-utility-methods.js
+++ dataTypes/arrays/arrayMethods/03-utility-methods.js
@@ -9,3 +9,16 @@
 // let result = a.concat(b);
 // console.log(result); // [1, 2, 3, 4]
 // console.log(a); // [1, 2] → original unchanged
+
+// 2. slice(start, end)
+// Returns a shallow copy of a portion of the array.
+
+// start: index to begin (inclusive)
+
+// end: index to end (exclusive)
+
+let arr = [10, 20, 30, 40];
+let part = arr.slice(1, 3);
+console.log(part); // [20, 30]
+console.log(arr);  // [10, 20, 30, 40]
+
