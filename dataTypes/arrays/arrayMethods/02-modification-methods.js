3cf0f094abdc226ed4d012949b4f1280c6b9acef
DagimDev <simedagim121@gmail.com>
2025-06-07 21:54:39 +0300

    MM pop()

--- dataTypes/arrays/arrayMethods/02-modification-methods.js
+++ dataTypes/arrays/arrayMethods/02-modification-methods.js
@@ -15,3 +15,22 @@ let colors = ['red', 'green'];
 colors.push('blue');
 
 console.log(colors); // ['red', 'green', 'blue']
+
+pop()
+// ➤ What it does:
+// Removes the last element from an array.
+
+// ➤ Syntax:
+
+array.pop();
+// ➤ Returns:
+// The removed element.
+
+// ➤ Mutates original array? ✅ Yes
+// ➤ Example:
+
+let colorsPop = ['red', 'green', 'blue'];
+let last = colors.pop();
+
+console.log(last);   // 'blue'
+console.log(colorsPop); // ['red', 'green']
