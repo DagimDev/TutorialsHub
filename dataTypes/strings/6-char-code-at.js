4478b3ad57532fd44a35c26bdd66ff2f668f9c15
DagimDev <simedagim121@gmail.com>
2025-06-09 21:19:41 +0300

    charCodeAt : Returns the Unicode code (number) of the character at index

---
+++ dataTypes/strings/6-char-code-at.js
@@ -0,0 +1,7 @@
+charCodeAt(index)
+// Returns the Unicode code (number) of the character at index.
+
+
+let str = "A";
+console.log(str.charCodeAt(0)); // 65
+// Useful for ASCII/Unicode manipulation.
