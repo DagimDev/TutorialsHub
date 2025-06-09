ebd972fa6cbbb7e2e9033871e38afcf5ff8aea99
DagimDev <simedagim121@gmail.com>
2025-06-09 21:10:27 +0300

    toLowerCase

--- dataTypes/strings/2-to-lower-upper-case.js
+++ dataTypes/strings/2-to-lower-upper-case.js
@@ -4,3 +4,12 @@
 
 // let msg = "hello";
 // console.log(msg.toUpperCase()); // "HELLO"
+
+
+// toLowerCase()
+// Converts all letters to lowercase.
+
+
+// let shout = "HELLO";
+// console.log(shout.toLowerCase()); // "hello"
+// 🔹 Useful when comparing text in a case-insensitive way.
