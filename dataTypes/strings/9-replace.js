0964f388132a43e050049a160664070693c1c635
DagimDev <simedagim121@gmail.com>
2025-06-09 21:26:45 +0300

    replace : Replaces part of a string with another

---
+++ dataTypes/strings/9-replace.js
@@ -0,0 +1,7 @@
+replace(search, replacement)
+// Replaces part of a string with another.
+
+let msg = "I love Java";
+console.log(msg.replace("Java", "JavaScript")); // "I love JavaScript"
+// You can also use regex for more complex patterns.
+
