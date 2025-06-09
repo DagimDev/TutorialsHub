03810192150844a0a45895e305376a389ce13664
DagimDev <simedagim121@gmail.com>
2025-06-09 21:12:30 +0300

    trim : Removes whitespace from both ends of a string

---
+++ dataTypes/strings/3-trim.js
@@ -0,0 +1,14 @@
+// 🧹 3. trim(), trimStart(), trimEnd()
+// trim()
+// Removes whitespace from both ends of a string.
+
+
+// let dirty = "   hi!  ";
+// console.log(dirty.trim()); // "hi!"
+// trimStart() / trimEnd()
+// Remove whitespace only from start or end:
+
+// let dirty = "   hi!  ";
+// console.log(dirty.trimStart()); // "hi!  "
+// console.log(dirty.trimEnd());   // "   hi!"
+// ✅ Does not change the string’s middle, only start and end.
