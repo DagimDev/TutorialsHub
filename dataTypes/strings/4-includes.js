94506b1604839b11acc3b34caae6203c0c7dc7d2
DagimDev <simedagim121@gmail.com>
2025-06-09 21:15:01 +0300

    includes :  Checks if a substring exists in a string. Returns boolean

---
+++ dataTypes/strings/4-includes.js
@@ -0,0 +1,9 @@
+// 🔍 4. includes(substring)
+// What it does:
+// Checks if a substring exists in a string. Returns true or false.
+
+let text = "I love JS!";
+console.log(text.includes("love")); // true
+console.log(text.includes("hate")); // false
+// Case-sensitive ("js" ≠ "JS")
+
