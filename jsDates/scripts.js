e3bed965dc5f55899d9dddc0572ec475efecce94
DagimDev <simedagim121@gmail.com>
2025-06-10 22:11:59 +0300

    new Date (Creating a Date Object)

---
+++ jsDates/scripts.js
@@ -0,0 +1,7 @@
+//1.
+ new Date() //– Creating a Date Object
+// Explanation: Creates a new Date object representing the current date and time.
+// Example:
+
+const now = new Date();
+console.log(now); // Output: Current date & time (e.g., "2023-10-05T12:34:56.789Z")
