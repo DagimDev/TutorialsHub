fd012fcadb74f374f1591b8f60e549048fa992f1
DagimDev <simedagim121@gmail.com>
2025-06-11 13:55:12 +0300

    Set.has Checks if a value exists in the Set (returns true or false)

--- CollectionObjects/set/common-set-methods.js
+++ CollectionObjects/set/common-set-methods.js
@@ -7,3 +7,12 @@ letters.add('a')
 letters.add('b')
 letters.add('a') // double values will be ignored
 console.log(letters)
+
+// 2. has(value)
+// Checks if a value exists in the Set (returns true or false).
+
+const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+console.log(typeof vowels)
+console.log(vowels.has('a'))
+console.log(vowels.has('x'))
+console.log(vowels)
