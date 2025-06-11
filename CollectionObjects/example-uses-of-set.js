7fdf052dcf5971d2657382330645df8e922ee7dc
DagimDev <simedagim121@gmail.com>
2025-06-11 14:17:38 +0300

    Set Ex Counting unique elements

--- CollectionObjects/example-uses-of-set.js
+++ CollectionObjects/example-uses-of-set.js
@@ -6,3 +6,11 @@ const uniqueNumbers = [...new Set(numbers)];
 // [1, 2, 3, 4, 5]
 console.log(numbers)
 console.log(uniqueNumbers)
+
+// Counting unique elements:
+
+const words = ['apple', 'banana', 'apple', 'orange'];
+const uniqueCount = new Set(words).size;
+// console.log(uniqueCount.size)
+console.log(uniqueCount)
+// 3
