b05e099276d383f8dfc8288fa0d590d154a130c4
DagimDev <simedagim121@gmail.com>
2025-06-11 17:55:13 +0300

    Summary of Everything Set Offers

--- CollectionObjects/set/set.js
+++ CollectionObjects/set/set.js
@@ -19,3 +19,13 @@ const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
 const hell = "hello";
 
 console.log(vowels)
+
+
+// Summary of Everything Set Offers
+// Feature	                    Details
+// Unique values	            Only stores unique elements.
+// Any type	                Supports primitives and objects.
+// Iterable	                Can loop with for...of, forEach, etc.
+// No key-value pairs	        Unlike Map, Set only stores values.
+// Conversion-friendly	        Easy to convert to/from arrays.
+// Useful for	                Removing duplicates, checking presence, simple set logic.
