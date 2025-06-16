65fe7ca7fd01db2489d8d09983455dba575c40e6
DagimDev <simedagim121@gmail.com>
2025-06-16 11:04:12 +0300

    Adding multiple listeners for a single event (in DOM level 2 event handler)

--- jsEvents/scipts.js
+++ jsEvents/scipts.js
@@ -18,3 +18,32 @@ addEventListener('click', changeColor)
 
 removeEventListener('click', changeColor)
 
+const myButton = document.getElementById('myButton')
+
+myButton.addEventListener("click", function () {
+myButton.style.color = "red";
+});
+myButton.addEventListener("click", function () {
+alert("button clicked!!!");
+});
+myButton.addEventListener("click", function () {
+myButton.style.backgroundColor = "yellow";
+});
+
+// console.log(myButton)
+
+// const changeColorToRed = () => {
+//     myButton.style.color = 'red'
+// }
+
+// const changebackgroudToYellow = () => {
+//     myButton.style.backgroundColor = 'yellow'
+// }
+
+// const clickAlert = () => {
+//     alert('button is clicked!!!')
+// }
+
+// myButton.addEventListener('click', clickAlert)
+// myButton.addEventListener('click', changebackgroudToYellow)
+// myButton.addEventListener('click', changeColorToRed)
