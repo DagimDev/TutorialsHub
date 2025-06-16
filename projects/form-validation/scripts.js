653558d0b332cd377791aae44a2efd492b5e23c3
DagimDev <simedagim121@gmail.com>
2025-06-16 22:12:21 +0300

    feat: Validate name field (non-empty check)

--- projects/form-validation/scripts.js
+++ projects/form-validation/scripts.js
@@ -7,4 +7,15 @@ const passwordErrorMsg = document.getElementById("passwordErrorMsg");
 button.addEventListener("click", (event) => {
   nameErrorMsg.innerHTML = "";
   passwordErrorMsg.innerHTML = "";
+  let isValid = true;
+  const nameValue = nameInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  // Validate name
+  if (nameValue === "") {
+    console.log("Name is empty");
+    nameInput.focus();
+    nameErrorMsg.innerHTML = "Name is required*";
+    isValid = false;
+  }
 });
