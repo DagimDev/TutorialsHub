f0585cbc1ab0bb287cd481f9fc18cdfa49619801
DagimDev <simedagim121@gmail.com>
2025-06-17 21:28:16 +0300

    Adding jquery to html file

---
+++ jQuery/scripts.js
@@ -0,0 +1,10 @@
+// Adding jQuery library to our web page
+// • There are two ways to include jQuery into your HTML
+// o You can download the latest version, save it locally in your computer and include
+// it in your HTML’s <header> using a simple <script > tag. Link to offcicial jQuery
+// website:
+// ▪ https://jquery.com/download/
+// o Including jQuery from a CDN and adding it right before the closing </body> in
+// <script> tag. Note: Your jQuery CDN needs to be placed before your custom
+// JavaScript file
+console.log("jQuery")
