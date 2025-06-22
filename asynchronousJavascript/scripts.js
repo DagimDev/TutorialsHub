57f73278981a826474331d874e816803a48fa65c
DagimDev <simedagim121@gmail.com>
2025-06-22 08:33:36 +0300

    Fetching data using promisses

--- asynchronousJavascript/scripts.js
+++ asynchronousJavascript/scripts.js
@@ -1,15 +1,22 @@
 console.log("Script loaded successfully!");
 // Callback Function
 function orderPizza(callback) {
-  console.log("Order Pizza")
+  console.log("Order Pizza");
   setTimeout(() => {
     const pizza = "🍕";
     console.log(`Your ${pizza} is Ready`);
     callback(pizza);
-  }, 2000);
+  }, 1000);
 }
 
 function pizzaReady(pizza) {
   console.log(`Eat Your ${pizza}`);
 }
 orderPizza(pizzaReady);
+
+// Example fetching data using promisses
+
+fetch("https://randomuser.me/api")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log("Something went wrong:", error));
