2a9d000646ab8dea048f22fb5b502983f07f021b
DagimDev <simedagim121@gmail.com>
2025-06-22 17:11:33 +0300

    URL Search Params

---
+++ constructors/web-specific-constructors/3-URLSearchParams.js
@@ -0,0 +1,35 @@
+// URLSearchParams
+// Definition:
+// Utility class for working with URL query strings.
+
+// Detailed Explanation:
+
+// javascript
+// const params = new URLSearchParams('?a=1&b=2');
+// params.append('c', '3');
+// console.log(params.toString()); // 'a=1&b=2&c=3'
+// Methods:
+
+// append() - Adds new parameter
+
+// delete() - Removes parameter
+
+// get()/getAll() - Gets parameter value(s)
+
+// has() - Checks if parameter exists
+
+// set() - Sets parameter value
+
+// Iterable:
+
+// Can use for...of to iterate
+
+// entries(), keys(), values() methods
+
+// Use Cases:
+
+// Building query strings
+
+// Parsing query parameters
+
+// Modifying existing URLs
