1a1edb6f2e8ee29c5d952554e89a257e3d5bdeba
DagimDev <simedagim121@gmail.com>
2025-06-24 14:24:07 +0300

    Test object of regexp

--- regex/scripts.js
+++ regex/scripts.js
@@ -9,5 +9,10 @@ console.log(str.search(regex));
 const teta = /teta/i
 console.log(str.replace(/bela/i, "teta"))
 
+// Mathch methos with regexp
 console.log(str.match("beso"))
 console.log(str.match(/bE/ig))
+
+// Test object of regexp
+console.log(/be/gi.test(str)) // returns true
+console.log(/teta/gi.test(str)) // returns false
