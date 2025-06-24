64be5c2adfaa5b7799a288a5cb47aaab055819b4
DagimDev <simedagim121@gmail.com>
2025-06-24 14:02:19 +0300

    Search method with regEx

--- regex/scripts.js
+++ regex/scripts.js
@@ -1,1 +1,7 @@
 console.log("Regular Expressions")
+// Using string search method
+const str = "Abebe beso bela!"
+console.log(str.search("beso"))
+// Using string search metho with regex
+const regex = /beso/g
+console.log(str.search(regex))
