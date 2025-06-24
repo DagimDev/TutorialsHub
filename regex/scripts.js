ebbc63cdc9ad5804f606dedafc2708feb4674a02
DagimDev <simedagim121@gmail.com>
2025-06-24 14:10:05 +0300

    Replace method with regex

--- regex/scripts.js
+++ regex/scripts.js
@@ -1,7 +1,10 @@
-console.log("Regular Expressions")
+console.log("Regular Expressions");
 // Using string search method
-const str = "Abebe beso bela!"
-console.log(str.search("beso"))
+const str = "Abebe beso bela!";
+console.log(str.search("beso"));
 // Using string search metho with regex
-const regex = /beso/g
-console.log(str.search(regex))
+const regex = /beso/g;
+console.log(str.search(regex));
+
+const teta = /teta/i
+console.log(str.replace(/bela/i, "teta"))
