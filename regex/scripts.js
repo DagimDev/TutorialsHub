37c50ea126fe814c04e4d54aff2d958be5119509
DagimDev <simedagim121@gmail.com>
2025-06-24 14:14:34 +0300

    Match method with regex

--- regex/scripts.js
+++ regex/scripts.js
@@ -1,6 +1,6 @@
 console.log("Regular Expressions");
 // Using string search method
-const str = "Abebe beso bela!";
+const str = "Abebe bEso beso bela!";
 console.log(str.search("beso"));
 // Using string search metho with regex
 const regex = /beso/g;
@@ -8,3 +8,6 @@ console.log(str.search(regex));
 
 const teta = /teta/i
 console.log(str.replace(/bela/i, "teta"))
+
+console.log(str.match("beso"))
+console.log(str.match(/bE/ig))
