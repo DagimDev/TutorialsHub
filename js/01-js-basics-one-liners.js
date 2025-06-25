ca3e6e703839b32f8a5015ab6f015593181d29e1
DagimDev <simedagim121@gmail.com>
2025-06-25 22:51:41 +0300

    Filter positive numbers

--- js/01-js-basics-one-liners.js
+++ js/01-js-basics-one-liners.js
@@ -15,3 +15,6 @@ const user = { name: "Alice", age: 25 };
 
 // 6. Check if number is even
 const isEven = n => n % 2 === 0;
+
+// 7. Filter positive numbers
+console.log([1, -2, 3, -4].filter(n => n > 0));
