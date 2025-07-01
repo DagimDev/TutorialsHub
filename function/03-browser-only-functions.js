590dbf819e8a9b7976a74e94e245c813edb23802
DagimDev <simedagim121@gmail.com>
2025-07-01 22:33:15 +0300

    Dialog Boxes

---
+++ function/03-browser-only-functions.js
@@ -0,0 +1,13 @@
+// 1. Dialog Boxes
+// alert()
+// Shows a popup message.
+
+alert("Warning!");
+confirm()
+// Shows a yes/no dialog, returns true/false.
+
+if (confirm("Delete?")) // { /* ... */ }
+prompt()
+// Asks for user input, returns the entered string.
+
+const name = prompt("Your name?");
