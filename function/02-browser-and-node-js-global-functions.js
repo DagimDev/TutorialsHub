0e1c4204aab62c9a0ede5e4f2983c1ce52206a5d
DagimDev <simedagim121@gmail.com>
2025-07-01 22:28:44 +0300

    Evaluation & Execution

--- function/02-browser-and-node-js-global-functions.js
+++ function/02-browser-and-node-js-global-functions.js
@@ -7,3 +7,14 @@ encodeURIComponent() / decodeURIComponent()
 // Encodes/decodes URI components (replaces more characters).
 
 encodeURIComponent("Hello & Goodbye!") // "Hello%20%26%20Goodbye%21"
+
+// 2. Evaluation & Execution
+// eval() (Avoid! Dangerous)
+// Executes a string as JS code.
+
+eval("2 + 2"); // 4
+Function() // (Safer than eval but still risky)
+// Creates a function from a string.
+
+const sum = new Function("a", "b", "return a + b");
+sum(2, 3); // 5
