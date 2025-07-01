18e2137cf60891782945ff9c9927ccd3cd6c5592
DagimDev <simedagim121@gmail.com>
2025-07-01 22:48:50 +0300

    Buffer Methods (Node.js-Only Functions)

--- function/04-node-js-only-functions.js
+++ function/04-node-js-only-functions.js
@@ -11,3 +11,9 @@ setImmediate(() => console.log("Runs soon"));
 process.nextTick() // (Runs before the next event loop)
 
 process.nextTick(() => console.log("Runs immediately"));
+
+// Buffer Methods
+
+// Handle binary data.
+
+Buffer.from("hello"); // <Buffer 68 65 6c 6c 6f>
