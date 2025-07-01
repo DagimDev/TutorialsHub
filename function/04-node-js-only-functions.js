746ee3b6a06b9d9f02342ad79fc3099f03aa2ebf
DagimDev <simedagim121@gmail.com>
2025-07-01 22:40:16 +0300

    Module Handling process.nextTick()

--- function/04-node-js-only-functions.js
+++ function/04-node-js-only-functions.js
@@ -7,3 +7,7 @@ const fs = require("fs");
 // setImmediate() (Runs after current event loop)
 
 setImmediate(() => console.log("Runs soon"));
+
+process.nextTick() // (Runs before the next event loop)
+
+process.nextTick(() => console.log("Runs immediately"));
