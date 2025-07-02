dcb3d8dafbb97225e8e4987ed7923272357497aa
DagimDev <simedagim121@gmail.com>
2025-07-02 20:56:36 +0300

    Add eval-based logic to compute final result

--- projects/calculator/scripts.js
+++ projects/calculator/scripts.js
@@ -1,6 +1,14 @@
- 
- let calculation = ""
- const updateCalculation = (value) => {
-    calculation += value;
-    console.log(calculation)
- }
+let calculation = "";
+const updateCalculation = (value) => {
+  calculation += value;
+  console.log(calculation);
+};
+
+function displayCalculation() {
+  document.querySelector(".display-calculation").innerHTML = calculation;
+}
+
+const calculateResult = () => {
+  calculation = eval(calculation);
+  console.log(calculation);
+};
