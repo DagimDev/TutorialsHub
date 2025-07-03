6f8f6988dddc4c5fe88717d986a8bc5637c2e936
DagimDev <simedagim121@gmail.com>
2025-07-03 12:42:28 +0300

    Mini calculator app

--- projects/calculator/scripts.js
+++ projects/calculator/scripts.js
@@ -1,23 +1,25 @@
 let calculation = JSON.parse(localStorage.getItem("calculation")) || "";
 const updateCalculation = (value) => {
   calculation += value;
-  console.log(calculation);
+  displayCalculation();
 };
 
 function displayCalculation() {
   document.querySelector(".display-calculation").innerHTML = calculation;
-  localStorage.setItem("calculation", JSON.stringify(calculation))
+  localStorage.setItem("calculation", JSON.stringify(calculation));
 }
 
 const calculateResult = () => {
   calculation = eval(calculation);
   console.log(calculation);
-  localStorage.setItem("calculation", JSON.stringify(calculation))
+  displayCalculation();
+  localStorage.setItem("calculation", JSON.stringify(calculation));
 };
 
-
 const clearCalculation = () => {
-  calculation = ""
-  localStorage.removeItem("calculation")
-  console.log(calculation)
-}
+  calculation = "";
+  localStorage.removeItem("calculation");
+  displayCalculation();
+  // console.log(calculation)
+};
+
