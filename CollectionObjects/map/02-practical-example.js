8660e7b64752e7cef0494a64bd9a84641b40ca37
DagimDev <simedagim121@gmail.com>
2025-07-05 23:22:24 +0300

    Add basic key-value pairs to Map

--- CollectionObjects/map/02-practical-example.js
+++ CollectionObjects/map/02-practical-example.js
@@ -1,3 +1,8 @@
 // Initialize new Map instance
 
 const userPreferences = new Map();
+
+// Add basic key-value pairs to Map
+
+userPreferences.set('theme', 'dark');
+userPreferences.set('language', 'en');
