b73a88a6e815af2391330cb3424dfc7c43979572
DagimDev <simedagim121@gmail.com>
2025-07-07 23:34:31 +0300

    Add click event handler

--- jQuery/17-jquery-learning-path.js
+++ jQuery/17-jquery-learning-path.js
@@ -1,3 +1,8 @@
 // 2: "Implement basic DOM selection"
 const $header = $('header');
 const $buttons = $('.btn');
+
+// 3: "Add click event handler"
+$('#login-btn').click(function() {
+  console.log('Button clicked!');
+});
