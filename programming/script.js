aa26ada3cda610ea19a2daae8a1c4c1d27b48ef0
DagimDev <simedagim121@gmail.com>
2025-07-09 22:20:35 +0300

    Programming in JavaScript: Definition

---
+++ programming/script.js
@@ -0,0 +1,12 @@
+// Programming in JavaScript: Definition
+// JavaScript programming involves creating dynamic web applications by:
+
+// Manipulating DOM - Changing webpage content/structure
+
+// Handling Events - Responding to user interactions
+
+// Managing Data - Working with objects, arrays, APIs
+
+// Asynchronous Operations - Using promises, async/await
+
+// Application State - Managing data flow and reactivity
