82331df18afec73aaeacc534fcd10a540fbc6b7d
DagimDev <simedagim121@gmail.com>
2025-07-13 23:11:32 +0300

    Install Cygwin base packages

---
+++ bashScript/cygwin_mastery.sh
@@ -0,0 +1,6 @@
+# Cygwin Mastery Tutorial - Windows Linux Environment
+
+### SECTION 1: CYGWIN SETUP ###
+# 1: "Install Cygwin base packages"
+# Run Cygwin Setup (setup-x86_64.exe) and select:
+# - curl, wget, git, openssh, tar, gzip
