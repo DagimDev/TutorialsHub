65ab3e61bd2a4a17f80bbe04c4a7bf66d270153e
DagimDev <simedagim121@gmail.com>
2025-07-15 21:32:37 +0300

    Add basic HTTP server

--- nodejs/nodejs_tutorial.js
+++ nodejs/nodejs_tutorial.js
@@ -7,3 +7,10 @@
 // 1: "Initialize Node.js project" =====
 // Run: npm init -y
 console.log("Project initialized");
+
+// 2: "Add basic HTTP server" =====
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Hello World\n');
+});
+server.listen(3000, () => console.log('Server running on port 3000'));
