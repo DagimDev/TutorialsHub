0ecebf2d3f49b3a9b1ebf408b24d75aba347ad49
DagimDev <simedagim121@gmail.com>
2025-07-21 23:33:18 +0300

    Add Redis caching layer

--- nodejs/node-js-mastery.js
+++ nodejs/node-js-mastery.js
@@ -24,3 +24,8 @@ const logger = winston.createLogger({
   ),
   transports: [new winston.transports.Console()]
 });
+
+// Add Redis caching layer
+import { createClient } from 'redis';
+const redisClient = createClient({ url: process.env.REDIS_URL });
+await redisClient.connect().catch(logger.error);
