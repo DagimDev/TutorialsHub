c05b2588ca2c35fc47c19a92fa9139fa1b48f97c
DagimDev <simedagim121@gmail.com>
2025-07-21 23:32:58 +0300

    Create custom logger with Winston

--- nodejs/node-js-mastery.js
+++ nodejs/node-js-mastery.js
@@ -13,3 +13,14 @@ console.log("DB URL:", process.env.DB_URL?.slice(0, 15) + "...");
 // Implement async error handling
 const asyncHandler = (fn) => (req, res, next) => 
   Promise.resolve(fn(req, res, next)).catch(next);
+
+// Create custom logger with Winston
+import winston from 'winston';
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [new winston.transports.Console()]
+});
