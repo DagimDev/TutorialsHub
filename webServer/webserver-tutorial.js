4d7dd37ddaaf4085850ba2ce44e5fb707e55f9bb
DagimDev <simedagim121@gmail.com>
2025-07-22 23:28:31 +0300

    Add server listening on port 3000

--- webServer/webserver-tutorial.js
+++ webServer/webserver-tutorial.js
@@ -5,3 +5,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
+
+// Add server listening on port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
